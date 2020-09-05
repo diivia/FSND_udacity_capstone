2c10dab9500870c29a2df0986ce9387a04d18355
Julija Kondratjeva <julija17@gmail.com>
2020-09-05 22:03:50 +0300

    update setup and readme

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 export AUTH0_DOMAIN='fsndjk.eu.auth0.com'
 export ALGORITHMS=['RS256']
 export API_AUDIENCE='capstone'
-export DATABASE_URL='postgresql://localhost:5432/capstone'
+export DATABASE_URL='postgres://oyqfnosqyelbxc:5b03cf67340611ebf6d0f782b0735fd8d013bbbc4c624cec7803270655491c47@ec2-54-166-107-5.compute-1.amazonaws.com:5432/d7gah31545sjds'
